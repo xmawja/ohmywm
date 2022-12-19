3e21696fb1ab71a4c86fcdaf85e8038e9902c43c
Muja Siyam <contact@muja.dev>
2022-12-19 11:56:29 +0100

    ADDED: SCReenshOT - command line screen capture utility, keybinding.
    scrot is a simple command line screen capture utility, it uses imlib2 to grab and save images.
    
    scrot has many useful features:
    
    Support for multiple image formats: JPG, PNG, GIF, and others.
    The screenshot's quality is configurable.
    It is possible to capture a specific window or a rectangular area on the screen.
    Because scrot is a command line utility, it can easily be scripted and put to novel uses. For instance, scrot can be used to monitor an X server in absence.
    
    scrot is free software under the MIT-advertising license.
    https://github.com/resurrecting-open-source-projects/scrot.git

--- config.def.h
+++ config.def.h
@@ -60,6 +60,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+/* scrot */
+static const char *scrotcmd[] = { "scrot", NULL};
 
 /* movestack patch*/
 #include "movestack.c"
@@ -68,6 +70,7 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = scrotcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
