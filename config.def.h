7c24acc046681ee5f458f7911df68a59fde66b3f
Muja Siyam <contact@muja.dev>
2022-12-24 20:49:44 +0100

    FIX: scrot glitsh when take a screenshot to selected area.

--- config.def.h
+++ config.def.h
@@ -73,7 +73,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ 0,            		XK_Print,  spawn,      	   {.v = scrotcmd } },
 	{ ShiftMask,    		XK_Print,  spawn,      	   {.v = scrotfocusedcmd } },
-	{ ControlMask,  		XK_Print,  spawn,    	   SHCMD("scrot --select") }, /*sleep 1s;*/
+	/* { ControlMask,  		XK_Print,  spawn,    	   SHCMD("scrot --select") }, /\*sleep 1s;*\/ */
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
