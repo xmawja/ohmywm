da6749ee5559b9fe30cf88c15eb50d288bc2b748
Muja Siyam <contact@muja.dev>
2023-04-24 13:05:56 +0200

    added emacs rule

--- config.def.h
+++ config.def.h
@@ -29,8 +29,10 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Emacs",    NULL,       NULL,       1 << 3,       0,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Steam",    NULL,       NULL,       1 << 7,       0,           -1 },
+	
 
 };
 
