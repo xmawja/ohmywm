dfdfe6ea1902f9d0244988a2be20a45477cf0c3f
Muja Siyam <contact@muja.dev>
2023-04-24 13:08:18 +0200

    added davinci resolve rule && fix tags mask

--- config.def.h
+++ config.def.h
@@ -29,7 +29,8 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Kdenlive", NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Kdenlive", NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Resolve",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Emacs",    NULL,       NULL,       1 << 3,       0,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Steam",    NULL,       NULL,       1 << 7,       0,           -1 },
